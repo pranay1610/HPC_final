b577ceacf99f3fab14f740413dae860f1b432715
Pranay Narsipuram <44880242+pranay1610@users.noreply.github.com>
2024-07-05 01:49:33 +0200

    Update plot_scaling.py

--- exercise2/analysis/plot_scaling.py
+++ exercise2/analysis/plot_scaling.py
@@ -1,43 +1,77 @@
-import matplotlib.pyplot as plt
 import os
+import re
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def parse_output(directory):
+# Function to read execution times from output files
+def read_execution_times(directory):
     times = []
-    for filename in sorted(os.listdir(directory)):
-        if filename.startswith("output_"):
-            with open(os.path.join(directory, filename)) as f:
+    tasks = []
+    for filename in os.listdir(directory):
+        if filename.startswith('output_'):
+            task_count = int(re.findall(r'\d+', filename)[0])
+            with open(os.path.join(directory, filename), 'r') as f:
                 for line in f:
                     if "Execution time" in line:
-                        time = float(line.split()[-1])
-                        times.append(time)
-    return times
+                        time_in_sec = float(re.findall(r'[\d.]+', line)[0])
+                        time_in_ms = time_in_sec * 1000
+                        tasks.append(task_count)
+                        times.append(time_in_ms)
+    return tasks, times
 
-# Strong scaling
-strong_scaling_dir = "results/mpi_strong_scaling"
-strong_scaling_times = parse_output(strong_scaling_dir)
-processes_strong = [1, 2, 4, 8, 16, 32]
+# Function to plot scaling data
+def plot_scaling(tasks, times, x_label, y_label, title, output_file):
+    df = pd.DataFrame({'Tasks': tasks, 'Time': times})
+    df.sort_values(by='Tasks', inplace=True)
+    plt.figure(figsize=(10, 6))
+    plt.plot(df['Tasks'], df['Time'], marker='o', linestyle='-', color='b')
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.grid(True)
+    plt.savefig(output_file)
+    plt.show()
 
-# Weak scaling
-weak_scaling_dir = "results/mpi_weak_scaling"
-weak_scaling_times = parse_output(weak_scaling_dir)
-processes_weak = [1, 2, 4, 8]
+# Read and plot MPI Strong Scaling
+mpi_strong_tasks, mpi_strong_times = read_execution_times('~/assignments/exercise2/results/mpi_strong_scaling/')
+plot_scaling(
+    mpi_strong_tasks,
+    mpi_strong_times,
+    'Number of MPI Tasks',
+    'Execution Time (ms)',
+    'MPI Strong Scaling',
+    '~/assignments/exercise2/images_plots/mpi_strong_scaling_plot.png'
+)
 
-# Plotting Strong Scaling
-plt.figure()
-plt.plot(processes_strong, strong_scaling_times, marker='o')
-plt.xlabel('Number of Processes')
-plt.ylabel('Execution Time (s)')
-plt.title('Strong Scaling')
-plt.grid(True)
-plt.savefig('results/strong_scaling.png')
+# Read and plot MPI Weak Scaling
+mpi_weak_tasks, mpi_weak_times = read_execution_times('~/assignments/exercise2/results/mpi_weak_scaling/')
+plot_scaling(
+    mpi_weak_tasks,
+    mpi_weak_times,
+    'Number of MPI Tasks',
+    'Execution Time (ms)',
+    'MPI Weak Scaling',
+    '~/assignments/exercise2/images_plots/mpi_weak_scaling_plot.png'
+)
 
-# Plotting Weak Scaling
-plt.figure()
-plt.plot(processes_weak, weak_scaling_times, marker='o')
-plt.xlabel('Number of Processes')
-plt.ylabel('Execution Time (s)')
-plt.title('Weak Scaling')
-plt.grid(True)
-plt.savefig('results/weak_scaling.png')
+# Read and plot OMP Strong Scaling
+omp_strong_tasks, omp_strong_times = read_execution_times('~/assignments/exercise2/results/omp_strong_scaling/')
+plot_scaling(
+    omp_strong_tasks,
+    omp_strong_times,
+    'Number of OpenMP Threads',
+    'Execution Time (ms)',
+    'OMP Strong Scaling',
+    '~/assignments/exercise2/images_plots/omp_strong_scaling_plot.png'
+)
 
-plt.show()
+# Read and plot OMP Weak Scaling
+omp_weak_tasks, omp_weak_times = read_execution_times('~/assignments/exercise2/results/omp_weak_scaling/')
+plot_scaling(
+    omp_weak_tasks,
+    omp_weak_times,
+    'Number of OpenMP Threads',
+    'Execution Time (ms)',
+    'OMP Weak Scaling',
+    '~/assignments/exercise2/images_plots/omp_weak_scaling_plot.png'
+)
